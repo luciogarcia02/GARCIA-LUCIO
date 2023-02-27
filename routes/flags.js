f23011f018ae0315e30d9a9a07606babfb2fd2f2
luciogarcia02 <luciogarcialam@gmail.com>
2023-02-27 20:38:25 -0300

    agrego capture by id

--- routes/flags.js
+++ routes/flags.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var flagsRepo = require('../src/repositories/flags')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+/* PATCH flag captured by id. */
+router.patch('/capture/:id', async function(req, res, next) {
+    if(!flagsRepo.flagExists(req.params.id)){
+        return res.status(400).json({
+            message: "FLAG_NOT_FOUND"
+          })
+    }
+
+    let data = await flagsRepo.captureFlagById(req.params.id);
+    console.log("LA BANDERA "+req.params.id+" HA SIDO CAPTURADA!!");
+    return res.status(200).json(data);
+  });
+
+
 
 module.exports = router;
