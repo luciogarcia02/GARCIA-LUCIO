a58262307889c76b1c1ab0cc48b4f26da69a9b72
luciogarcia02 <luciogarcialam@gmail.com>
2023-02-27 21:45:48 -0300

    subo repo flags

--- src/repositories/flags.js
+++ src/repositories/flags.js
@@ -7,12 +7,57 @@ async function flagExists(id){
     }) == 1
   }
 
+  async function getFlags(){
+    return await Base.findAll()
+  }
+
+  async function getFlagById(id){
+    return await Flag.findByPk(id)
+  }
+
+  async function setBaseId(id, baseIdParam){
+    let myFlag = getFlagById(id);
+    myFlag.baseId= baseIdParam;
+    myFlag.save();
+    return myFlag;
+  }
 
 async function captureFlagById(id){
-    return ;
+    let myFlag = Flag.findByPk(id);
+    myFlag.isCaptured=true;
+    myFlag.baseId=0;
+    await myFlag.save();
+    return myFlag;
 }
 
+async function flagIsCaptured(id){
+    let myFlag = Flag.findByPk(id);
+    return myFlag.isCaptured;
+}
+
+async function settleFlag(baseId,flagId){
+    return setBaseId(flagId, baseId);
+}
+
+async function sameBaseId(baseId,flagId){
+    ok=true;
+    let miarray = getFlags();
+    let baseValor = miarray[0].baseId;
+    for (let i = 0; i < miarray.length; i++) {
+        if(miarray[0].baseId != baseValor){
+            ok=false
+        }
+
+      }
+    
+    return ok;
+}
 module.exports = {
     flagExists,
-    captureFlagById
+    captureFlagById,
+    flagIsCaptured,
+    setBaseId,
+    getFlags,
+    settleFlag,
+    sameBaseId,
 }
